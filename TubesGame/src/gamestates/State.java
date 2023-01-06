8aafc9b737922362386934641b732dbc6c0e443b
Froyotive <evandanirafif@gmail.com>
2023-01-06 09:34:46 +0700

    ...

--- TubesGame/src/gamestates/State.java
+++ TubesGame/src/gamestates/State.java
@@ -1,22 +1,35 @@
 package gamestates;
 
+import java.awt.event.MouseEvent;
+
+import audio.AudioPlayer;
 import main.Game;
 import ui.MenuButton;
 
-import java.awt.event.MouseEvent;
-
 public class State {
 
-    protected Game game;
+	protected Game game;
 
-    public State(Game game) {
-        this.game = game;
-    }
-    public boolean isIn(MouseEvent e, MenuButton mb){
-        return mb.getBounds().contains(e.getX(),e.getY());
-    }
+	public State(Game game) {
+		this.game = game;
+	}
 
-    public Game getGame() {
-        return game;
-    }
+	public boolean isIn(MouseEvent e, MenuButton mb) {
+		return mb.getBounds().contains(e.getX(), e.getY());
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	public void setGamestate(Gamestate state) {
+		switch (state) {
+		case MENU -> game.getAudioPlayer().playSong(AudioPlayer.MENU_1);
+		case PLAYING -> game.getAudioPlayer().setLevelSong(game.getPlaying().getLevelManager().getLevelIndex());
+		}
+
+		Gamestate.state = state;
+	}
+
 }
--- TubesGame/src/main/GameWindow.java
+++ TubesGame/src/main/GameWindow.java
@@ -1,35 +1,38 @@
 package main;
 
-import javax.swing.JFrame;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
-import java.awt.event.WindowStateListener;
+
+import javax.swing.JFrame;
 
 public class GameWindow {
-    private JFrame jframe;
+	private JFrame jframe;
 
-    public GameWindow(GamePanel gamePanel){
+	public GameWindow(GamePanel gamePanel) {
 
-        jframe = new JFrame();
+		jframe = new JFrame();
 
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.add(gamePanel);
 
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.add(gamePanel);
-        jframe.setLocationRelativeTo(null);
-        jframe.setResizable(false);
-        jframe.pack();
-        jframe.setVisible(true);
-        jframe.addWindowFocusListener(new WindowFocusListener() {
-            @Override
-            public void windowGainedFocus(WindowEvent e) {
+		jframe.setResizable(false);
+		jframe.pack();
+		jframe.setLocationRelativeTo(null);
+		jframe.setVisible(true);
+		jframe.addWindowFocusListener(new WindowFocusListener() {
 
-            }
+			@Override
+			public void windowLostFocus(WindowEvent e) {
+				gamePanel.getGame().windowFocusLost();
+			}
 
-            @Override
-            public void windowLostFocus(WindowEvent e) {
-                gamePanel.getGame().windowFocusLost();
+			@Override
+			public void windowGainedFocus(WindowEvent e) {
+				// TODO Auto-generated method stub
 
-            }
-        });
-    }
+			}
+		});
+
+	}
+
 }
